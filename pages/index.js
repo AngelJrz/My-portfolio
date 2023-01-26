e72c47aa3768a1bd72e8ff4a90366532c164372f
AngelJrz <angel_is2020@hotmail.com>
2023-01-25 22:20:55 -0600

    First changes

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Box } from "@chakra-ui/react"
+const Page = () => {
+	return(
+		<Container> 
+			<Box borderRadius="lg" bg='red' p={3} mb={6} align="center">
+				Hello, I&apos;m a software engineer from México!
+			</Box>
+		</Container>
+
+	)
+}
+
+export default Page
