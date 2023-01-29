74fa7b7af5179d2f8747782de5a08bd41696a24e
AngelJrz <angel_is2020@hotmail.com>
2023-01-28 19:13:51 -0600

    Navbar added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import { Container, Box } from "@chakra-ui/react"
+import { Container, Box, Heading } from "@chakra-ui/react"
 const Page = () => {
 	return(
 		<Container> 
 			<Box borderRadius="lg" bg='red' p={3} mb={6} align="center">
 				Hello, I&apos;m a software engineer from México!
+			</Box>
+
+			<Box display={{md: 'flex'}}>
+				<Box flexGrow={1}>
+					<Heading as="h2" variant="page-title">
+						AngelJrz
+					</Heading>
+					<p>Digital Craftzman (artist, developer)</p>
+				</Box>
 			</Box>
 		</Container>
 
