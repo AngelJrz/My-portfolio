26af686351bfd3b847da33ed8f4b52816fe28eec
AngelJrz <angel_is2020@hotmail.com>
2023-02-02 22:31:42 -0600

    Profile image added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, chakra, useColorModeValue } from "@chakra-ui/react"
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
 const Page = () => {
 	return(
 		<Container> 
-			<Box borderRadius="lg" bg='red' p={3} mb={6} align="center">
+			<Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
 				Hello, I&apos;m a software engineer from México!
 			</Box>
 
@@ -12,6 +16,26 @@ const Page = () => {
 						AngelJrz
 					</Heading>
 					<p>Digital Craftzman (artist, developer)</p>
+				</Box>
+				<Box flexShrink={0} mt={{base: 4, md:0}} ml={{md:6}} align="center">
+					<Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/profilepic.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100"
+              height="100"
+            />
+          </Box>
 				</Box>
 			</Box>
 		</Container>
